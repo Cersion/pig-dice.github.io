af8cf08715d253f43565f2b61e50264e989711d9
nicole wakio <nicolewakio@gmail.com>
2019-03-05 13:07:34 +0300

    javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //back-end
 var scorePlayerOne=[]
-var scorrePlayerTwo=[]
+var scorePlayerTwo=[]
 
 var rollDiceOne=()=>{
   var num = Math.floor((Math.random() * 6) + 1);
@@ -24,17 +24,52 @@ var rollDiceTwo=()=>{
     if(num2===1)
     {
       alert(num2);
-      addResultOne();
+      addResultTwo();
     }
     else
     {
       alert(num2);
-      scorePlayerOne.push(num2);
-      var show2=document.getElementById("live2")
+      scorePlayerTwo.push(num2);
+      var show2=document.getElementById("live2");
       show2.innerHTML += "<li>"+num2+"</li>";
 
     }
 
+}
+var addResultOne=()=>{
+  var result= eval(scorePlayerOne.join('+'));
+  if(result >100 || result===100)
+  {
+    alert("player1 won");
+    document.getElementById("actualcount1").textContent="100";
+    $(".buttonbox").hide();
+    location.reload();
+  }
+  else if (result===undefined)
+  {
+    alert("you rolled a one");
+  }
+  else {
+    document.getElementById("actualcount1").textContent=result
+  }
+}
+
+var addResultTwo=()=>{
+  var result2= eval(scorePlayerTwo.join('+'));
+  if(result2 >100 || result2===100)
+  {
+    alert("player2 won");
+    document.getElementById("actualcount2").textContent="100";
+    $(".buttonbox").hide();
+    location.reload();
+  }
+  else if (result2===undefined)
+  {
+    alert("you rolled a one");
+  }
+  else {
+    document.getElementById("actualcount2").textContent=result2
+  }
 }
 
 
